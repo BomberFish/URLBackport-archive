3691e41e0022adfb299e205a6b02c3a60f8a5718
woxtu <woxtup@gmail.com>
2022-12-17 00:13:42 +0900

    Initialize a file URL

--- Sources/URLBackport/URLBackport.swift
+++ Sources/URLBackport/URLBackport.swift
@@ -1,5 +1,49 @@
-public struct URLBackport {
-  public private(set) var text = "Hello, World!"
+import Foundation
 
-  public init() {}
+public extension URL {
+  struct Backport {
+    private let base: URL
+
+    init(
+      base: URL
+    ) {
+      self.base = base
+    }
+  }
+
+  static var backport: Backport.Type { Backport.self }
+  var backport: Backport { Backport(base: self) }
+
+  static func backport(filePath path: String, directoryHint: URL.Backport.DirectoryHint = .inferFromPath, relativeTo base: URL? = nil) -> URL {
+    return URL(fileURLWithPath: path, isDirectory: path.isDirectory(hint: directoryHint), relativeTo: base)
+  }
+}
+
+public extension URL.Backport {
+  enum DirectoryHint: Equatable, Hashable, Sendable {
+    case isDirectory
+    case notDirectory
+    case checkFileSystem
+    case inferFromPath
+  }
+}
+
+private extension String {
+  func isDirectory(hint: URL.Backport.DirectoryHint) -> Bool {
+    switch hint {
+    case .isDirectory:
+      return true
+
+    case .notDirectory:
+      return false
+
+    case .checkFileSystem:
+      var isDirectory: ObjCBool = false
+      FileManager.default.fileExists(atPath: self, isDirectory: &isDirectory)
+      return isDirectory.boolValue
+
+    case .inferFromPath:
+      return hasSuffix("/")
+    }
+  }
 }
--- Tests/URLBackportTests/URLBackportTests.swift
+++ Tests/URLBackportTests/URLBackportTests.swift
@@ -1,11 +1,53 @@
 @testable import URLBackport
 import XCTest
 
+@available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *)
 final class URLBackportTests: XCTestCase {
-  func testExample() throws {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct
-    // results.
-    XCTAssertEqual(URLBackport().text, "Hello, World!")
+  func testInitializeWithDirectoryPath() {
+    let path = "/path"
+    XCTAssertEqual(
+      URL(filePath: path, directoryHint: .isDirectory, relativeTo: nil),
+      URL.backport(filePath: path, directoryHint: .isDirectory, relativeTo: nil)
+    )
+  }
+
+  func testInitializeWithFilePath() {
+    let path = "/path"
+    XCTAssertEqual(
+      URL(filePath: path, directoryHint: .notDirectory, relativeTo: nil),
+      URL.backport(filePath: path, directoryHint: .notDirectory, relativeTo: nil)
+    )
+  }
+
+  func testInitializeWithExistingFilePath() {
+    let path = NSHomeDirectory()
+    XCTAssertEqual(
+      URL(filePath: path, directoryHint: .checkFileSystem, relativeTo: nil),
+      URL.backport(filePath: path, directoryHint: .checkFileSystem, relativeTo: nil)
+    )
+  }
+
+  func testInitializeWithNotExistingFilePath() {
+    let path = "/path"
+    XCTAssertEqual(
+      URL(filePath: path, directoryHint: .checkFileSystem, relativeTo: nil),
+      URL.backport(filePath: path, directoryHint: .checkFileSystem, relativeTo: nil)
+    )
+  }
+
+  func testInitializeWithFilePathHavingTrailingSlash() {
+    let path = "/path/"
+    XCTAssertEqual(
+      URL(filePath: path, directoryHint: .inferFromPath, relativeTo: nil),
+      URL.backport(filePath: path, directoryHint: .inferFromPath, relativeTo: nil)
+    )
+  }
+
+  func testInitializeWithFilePathNotHavingTrailingSlash() {
+    let path = "/path"
+    XCTAssertEqual(
+      URL(filePath: path, directoryHint: .inferFromPath, relativeTo: nil),
+      URL.backport(filePath: path, directoryHint: .inferFromPath, relativeTo: nil)
+    )
   }
 }
